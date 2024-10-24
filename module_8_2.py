5edb2e12ab0a8f4679f3105cf8d4248c7fd8eef0
ciberSi4 <alex.baikonur@mail.ru>
2024-10-24 20:11:48 +0300

    Add files via upload

---
+++ module_8_2.py
@@ -0,0 +1,28 @@
+# Домашнее задание по теме "Сложные моменты и исключения в стеке вызовов функции"
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+    for i in numbers:
+        try:
+            result += i
+        except TypeError:
+            incorrect_data += 1
+            print(f"Некорректный тип данных для подсчёта суммы - {i}")
+    return result, incorrect_data
+
+
+def calculate_average(numbers):
+    try:
+        the_amount = personal_sum(numbers)
+        arithmetic_mean = the_amount[0] / (len(numbers) - the_amount[1])
+        return arithmetic_mean
+    except ZeroDivisionError:
+        return 0
+    except TypeError:
+        print("В numbers записан некорректный тип данных")
+
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}')  # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')  # Всё должно работать
